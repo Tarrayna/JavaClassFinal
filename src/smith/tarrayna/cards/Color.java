9a6e16811248cfa4043ee15610d81357696e36ba
tarrayna <tarraynamg15@gmail.com>
2021-05-12 17:03:59 -0700

    Removed a TODO

--- src/smith/tarrayna/cards/Color.java
+++ src/smith/tarrayna/cards/Color.java
@@ -7,7 +7,6 @@ package smith.tarrayna.cards;
  * Final Project
  */
 public enum Color {
-    //TODO: Add the color ASCII as a variable Please!!
     RED("\u001b[31m"), YELLOW("\u001b[33m"), GREEN("\u001b[32m"), BLUE("\u001b[34m"), CLEAR("\u001b[0m"), BLACK("\u001b[30m");
 
     private final String colorCode;
